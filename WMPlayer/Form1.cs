c96cadd9aa44ce4cdc342ed051f58de3e08c2e48
Dmitriy Solopov <div.solopov@gmail.com>
2021-03-31 20:51:54 +0300

    first commit

---
+++ WMPlayer/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace WMPlayer
+{
+    public partial class MainForm : Form
+    {
+        private PlayListForm _playListForm;
+      
+
+        public MainForm()
+        {
+            InitializeComponent();
+            aboutUsToolStripMenuItem.Click += AboutUsToolStripMenuItem_Click;
+        }
+
+        private void AboutUsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutUsForm aboutUsForm = new AboutUsForm();
+            // aboutUsForm.Show();  - немодальное окно
+            aboutUsForm.ShowDialog(); // - модальное окно
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            string mediaPath = Assembly.GetExecutingAssembly().Location;
+            mediaPath = Path.GetDirectoryName(mediaPath) + "\\Sabaton.mp4";
+
+            axWindowsMediaPlayer.URL = mediaPath;
+            toolStripStatusLabel_Info.Text = Path.GetFileName( mediaPath);
+            toolStripStatusLabel_Info.ForeColor = Color.Red;
+
+
+           
+        }
+
+
+        private void playlistToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(_playListForm == null)
+            {
+                _playListForm = new PlayListForm();
+                _playListForm.Location = 
+                        new Point(this.Location.X + this.Size.Width - 14, this.Location.Y);
+                _playListForm.Size = new Size(_playListForm.Width, this.Height);
+            }
+            _playListForm.Show();
+        }
+
+       
+
+        private void toolStripStatusLabel_Info_Click(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
+
+
+
+
+
+
+// case SQLIteMode.NEW:
+//                    {
+//    SQLiteConnector.CreateDatabaseSource(dbPath);
+//    //развернуть дб по заранее заложенному алгоритму
+//    string query = "CREATE TABLE PlayLists5 (" +
+//        "Id INTEGER NOT NULL, Title NUMERIC," +
+//        "FilePath  NUMERIC NOT NULL," +
+//        "Artist  TEXT, " +
+//        "Album TEXT, " +
+//        "TrackNumber INTEGER, " +
+//        "Date TEXT," +
+//        "Duration  NUMERIC," +
+//        "Codec TEXT," +
+//        "Playing   INTEGER," +
+//        "FileName  TEXT," +
+//        "FileExtension TEXT," +
+//        "FileSize  NUMERIC," +
+//        "Bitrate   INTEGER," +
+//        "PRIMARY KEY(Id AUTOINCREMENT) ) ";
+//    Console.WriteLine(query);
+//    SQLiteConnector.Connection.Open();
+//    SQLiteCommand sQLiteCommand = new SQLiteCommand(query, SQLiteConnector.Connection);
+//    sQLiteCommand.ExecuteNonQuery();
+
+//    break;
+//}
---
+++ WMPlayer/PlayListForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SQLiteORM;
+
+namespace WMPlayer
+{
+    public partial class PlayListForm : Form
+    {
+
+        private SQLiteDBEngine dBEngine;
+
+        public PlayListForm()
+        {
+            InitializeComponent();
+        }
+        private void PlayListForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.Hide();
+            e.Cancel = true;
+        }
+
+        private void button_Add_Treck_Click(object sender, EventArgs e)
+        {
+            database();
+            SQLiteTable Students = dBEngine["student"];           
+            foreach (var col in Students.BodyRows)
+            {              
+                foreach (var item in col.Value)
+                {                    
+                    listBox_Playlists.Items.Add(item);
+                }
+            }
+           
+          
+            
+        }
+
+        private void database()
+        {
+            /*string pathTofile = */
+            Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "database", "test.db");
+            // string pathTofile = @"D:\c#\lesson11\SQLiteDatabaseBrowserPortable\PlayList1.db";
+            //string pathTofile = @"D:\c#\lesson19\SQLiteORMCRUDNewMethod\SQLiteORM\SQLiteORM\bin\Debug\database\test.db";
+            string pathTofile = @" D:\c#\lesson27\WMPlayer\WMPlayer\bin\Debug\test.db";
+            dBEngine = new SQLiteDBEngine(pathTofile, SQLIteMode.EXISTS);
+
+
+        }
+
+     
+    }
+}
